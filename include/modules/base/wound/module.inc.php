0996a0f4ba6b8940a803c984fc7628890142020b
winddramon <ranshiin@qq.com>
2024-02-21 18:54:39 +0800

    ui update 2

--- include/modules/base/wound/module.inc.php
+++ include/modules/base/wound/module.inc.php
@@ -7,7 +7,7 @@ namespace wound
 	$___MODULE_dependency_optional = 'battle weapon metman';
 	$___MODULE_conflict = '';
 	$___MODULE_codelist = 'main.php config/wound.config.php';
-	$___MODULE_templatelist = 'wound_profile wound_picture';
+	$___MODULE_templatelist = 'wound_profile wound_profile_battle wound_picture';
 	////////// MODULE HEADER END /////////////////
 	require __INIT_MODULE__(__NAMESPACE__,__DIR__);
 }
