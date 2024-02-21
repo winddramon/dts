13467d411af0e7e431970969035b6efd08fbc89e
winddramon <ranshiin@qq.com>
2024-02-21 23:56:59 +0800

    some fix

--- include/roommng/roommng.config.php
+++ include/roommng/roommng.config.php
@@ -374,7 +374,7 @@ $roomtypelist = Array(
 			0 => 'ff0022',
 		),
 		'teamID' => Array(	//队伍名，只需对队长设置即可
-			0 => '挑战者',
+			0 => '',
 		),
 		'show-team-leader' => 0,	//是否显示“队长”标签（如队伍大于1人设为1）
 		'card' => array(
@@ -580,7 +580,7 @@ $roomtypelist = Array(
 			0 => 'ff0022',
 		),
 		'teamID' => Array(	//队伍名，只需对队长设置即可。
-			0 => '试炼者',
+			0 => '',
 		),
 		'show-team-leader' => 0,	//是否显示“队长”标签（如队伍大于1人设为1）
 		'game-option' => array(
