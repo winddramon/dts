bac45d2218aba5c3ba959e2fe8c1cdc0906aa13c
winddramon <ranshiin@qq.com>
2024-03-03 22:58:26 +0800

    bufficons update : skill247 updated

--- include/modules/extra/club/skills/skill247/main.php
+++ include/modules/extra/club/skills/skill247/main.php
@@ -7,15 +7,19 @@ namespace skill247
 	function init() 
 	{
 		define('MOD_SKILL247_INFO','card;club;upgrade;');
-		eval(import_module('clubbase'));
+		eval(import_module('clubbase','bufficons'));
 		$clubskillname[247] = '挖坑';
+		$bufficons_list[247] = Array(
+			'dummy' => 1,
+		);
 	}
 	
 	function acquire247(&$pa)
 	{
 		if (eval(__MAGIC__)) return $___RET_VALUE;
 		eval(import_module('sys','skill247'));
-		\skillbase\skill_setvalue(247,'lastuse',-3000,$pa);
+		\skillbase\skill_setvalue(247,'end_ts',$now-1,$pa);
+		\skillbase\skill_setvalue(247,'cd_ts',$now+$skill247_cd,$pa);
 	}
 	
 	function lost247(&$pa)
@@ -43,7 +47,7 @@ namespace skill247
 	{
 		if (eval(__MAGIC__)) return $___RET_VALUE;
 		eval(import_module('sys','player'));
-		$skill247_lst = (int)\skillbase\skill_getvalue(247,'lastuse'); 
+		$skill247_lst = (int)\skillbase\skill_getvalue(247,'end_ts'); 
 		$skill247_time = $now-$skill247_lst; 
 		return round(min($skill247_time/60,3)*$att);
 	}
@@ -53,24 +57,12 @@ namespace skill247
 		if (eval(__MAGIC__)) return $___RET_VALUE;
 		eval(import_module('skill247','player','logger','sys'));
 		\player\update_sdata();
-		if (!\skillbase\skill_query(247) || !check_unlocked247($sdata))
-		{
-			$log.='你没有这个技能！<br>';
+		list($is_successful, $fail_hint) = \bufficons\bufficons_activate_buff(247, 0, $skill247_cd);
+		if(!$is_successful) {
+			$log .= $fail_hint;
 			return;
 		}
-		$st = check_skill247_state($sdata);
-		if ($st==0){
-			$log.='你不能使用这个技能！<br>';
-			return;
-		}
-		if ($st==1){
-			$log.='你已经发动过这个技能了！<br>';
-			return;
-		}
-		if ($st==2){
-			$log.='技能冷却中！<br>';
-			return;
-		}
+		$log.='<span class="lime b">技能「挖坑」发动成功。</span><br>';
 		$itm_name_arr = Array('毒性【ACFUN大逃杀3.0】', '毒性【ACFUN大逃杀革新企划书】', '毒性【大逃杀新界面】', '毒性【新电波大逃杀剧情模式】','有毒的腿？');
 		$itme = get_skill247_trap_eff();
 		for($i=0;$i<2;$i++){
@@ -85,41 +77,17 @@ namespace skill247
 			\trap\trap_use($trapitem); 
 		}
 		addnews ( 0, 'bskill247', $name );
-		$log.='你满意地看着你刚挖的大坑，它们一定能给玩家们带来笑容。<br><span class="lime b">技能「挖坑」发动成功。</span><br>';
-		\skillbase\skill_setvalue(247,'lastuse',$now);
+		$log.='你满意地看着你刚挖的大坑，它们一定能给玩家们带来笑容。<br>';
 	}
-	
-	function bufficons_list()
-	{
+
+	//提示文字的重载
+	function bufficons_display_single($token, $config, &$pa=NULL) {
 		if (eval(__MAGIC__)) return $___RET_VALUE;
-		eval(import_module('sys','player'));
-		\player\update_sdata();
-		if ((\skillbase\skill_query(247,$sdata))&&check_unlocked247($sdata))
-		{
-			eval(import_module('skill247'));
-			$skill247_lst = (int)\skillbase\skill_getvalue(247,'lastuse'); 
-			$skill247_time = $now-$skill247_lst; 
-			$sk247_eff = get_skill247_trap_eff();
-			$z=Array(
-				'disappear' => 0,
-				'clickable' => 1,
-				'hint' => '技能「挖坑」',
-				'activate_hint' => '点击发动技能「挖坑」<br>在当前地图布设2个'.$sk247_eff.'效果的毒性陷阱',
-				'onclick' => "$('mode').value='special';$('command').value='skill247_special';$('subcmd').value='activate';postCmd('gamecmd','command.php');this.disabled=true;",
-			);
-			if ($skill247_time<$skill247_cd)
-			{
-				$z['style']=2;
-				$z['totsec']=$skill247_cd;
-				$z['nowsec']=$skill247_time;
-			}
-			else 
-			{
-				$z['style']=3;
-			}
-			\bufficons\bufficon_show('img/skill247.gif',$z);
+		list($src, $config_ret) = $chprocess($token, $config, $pa);
+		if(247 == $token && \skillbase\skill_query(247,$pa) && check_unlocked247($pa)) {
+			$config_ret['activate_hint'] = '点击发动技能「挖坑」：<br>在当前地图布设2个'.get_skill247_trap_eff().'效果的毒性陷阱';
 		}
-		$chprocess();
+		return Array($src, $config_ret);
 	}
 	
 	function get_traplist() 	//不会踩自己的陷阱
