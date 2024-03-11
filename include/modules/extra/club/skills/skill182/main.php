9d81a3eab30daf7171abed9e7775deb3d17c0cd0
winddramon <ranshiin@qq.com>
2024-03-11 10:10:31 +0800

    bufficons update part23
    就差药了

--- include/modules/extra/club/skills/skill182/main.php
+++ include/modules/extra/club/skills/skill182/main.php
@@ -2,11 +2,18 @@
 
 namespace skill182
 {
+	$tmp_hint182 = '';//用于暂存提示信息
+
 	function init() 
 	{
 		define('MOD_SKILL182_INFO','club;hidden;');
-		eval(import_module('clubbase'));
+		eval(import_module('clubbase','bufficons'));
 		$clubskillname[182] = '共鸣';
+		$bufficons_list[182] = Array(
+			'disappear' => 1,
+			'clickable' => 1,
+			'onclick' => '',
+		);
 	}
 	
 	function acquire182(&$pa)
@@ -15,6 +22,7 @@ namespace skill182
 		\skillbase\skill_setvalue(182, 'skey', '', $pa);
 		\skillbase\skill_setvalue(182, 'svalue', '', $pa);
 		\skillbase\skill_setvalue(182, 'stime', '', $pa);
+		\skillbase\skill_setvalue(182, 'sstart', 0, $pa);
 	}
 	
 	function lost182(&$pa)
@@ -27,7 +35,9 @@ namespace skill182
 		if (eval(__MAGIC__)) return $___RET_VALUE;
 		eval(import_module('sys'));
 		$expire = $now + $bufftime;
-		if (!\skillbase\skill_query(182, $pa)) \skillbase\skill_acquire(182, $pa);
+		if (!\skillbase\skill_query(182, $pa)) {
+			\skillbase\skill_acquire(182, $pa);
+		}
 		$skey = \skillbase\skill_getvalue(182, 'skey', $pa);
 		$svalue = \skillbase\skill_getvalue(182, 'svalue', $pa);
 		//此处的time是结束时间
@@ -44,6 +54,7 @@ namespace skill182
 			$lsvalue = explode('_', $svalue);
 			$lstime = explode('_', $stime);
 		}
+		if(empty($lstime) || $now > max($lstime)) $newflag = 1;
 		$lskey[] = $key;
 		$lsvalue[] = $value;
 		$lstime[] = $expire;
@@ -53,48 +64,64 @@ namespace skill182
 		\skillbase\skill_setvalue(182, 'skey', $skey, $pa);
 		\skillbase\skill_setvalue(182, 'svalue', $svalue, $pa);
 		\skillbase\skill_setvalue(182, 'stime', $stime, $pa);
+		if(!empty($newflag)) {//新获得技能时记录开始时间
+			\skillbase\skill_setvalue(182, 'sstart', $now, $pa);
+		}
 		
 		$s = "持续时间<span class=\"yellow b\">".$bufftime."</span>秒";
 		return $s;
 	}
-	
-	function bufficons_list()
+
+	//歌系统有自己的一套实现。但歌部分暂时不支持按毫秒计算
+	function bufficons_check_buff_state($token, &$pa=NULL, $msec=0)
 	{
 		if (eval(__MAGIC__)) return $___RET_VALUE;
-		eval(import_module('sys','player'));
-		\player\update_sdata();		
-		if (\skillbase\skill_query(182,$sdata))
-		{
-			$skey = \skillbase\skill_getvalue(182, 'skey', $sdata);
+		$ret = $chprocess($token, $pa, $msec);
+		if(182 == $token && \skillbase\skill_query(182,$pa)) {
+			eval(import_module('sys','bufficons','skill182'));
+			$skey = \skillbase\skill_getvalue(182, 'skey', $pa);
 			if (!empty($skey))
 			{
-				$svalue = \skillbase\skill_getvalue(182, 'svalue', $sdata);
-				$stime = \skillbase\skill_getvalue(182, 'stime', $sdata);
+				$svalue = \skillbase\skill_getvalue(182, 'svalue', $pa);
+				$stime = \skillbase\skill_getvalue(182, 'stime', $pa);
 				$lskey = explode('_', $skey);
 				$lsvalue = explode('_', $svalue);
-				$lstime = explode('_', $stime);			
+				$lstime = explode('_', $stime);
 				$tmax = max($lstime) - $now;
-				$z = array(
-					'disappear' => 1,
-				);
 				if ($tmax > 0)
 				{
-					$z['clickable'] = 1;
-					$z['style'] = 1;
-					$z['totsec'] = $tmax;
-					$z['nowsec'] = 0;
-					$z['hint'] = show_songbuff_hint($lskey, $lsvalue);
+					$ret = 1;
+					$sstart = \skillbase\skill_getvalue(182, 'sstart', $pa);
+					if(!empty($sstart)) {
+						$tmp_totsec = max($lstime) - $sstart;
+						$tmp_nowsec = $now - $sstart;
+					}else{
+						$tmp_totsec = $tmax;
+						$tmp_nowsec = 0;
+					}
+					//这里为了省一步get，放到check_state()函数里
+					$tmp_hint182 = show_songbuff_hint($lskey, $lsvalue);
 				}
 				else
 				{
-					$z['clickable'] = 0;
-					$z['style'] = 3;
-					$z['activate_hint'] = "歌唱效果已经结束";
+					$ret = 3;
 				}
-				\bufficons\bufficon_show('img/skill182.gif',$z);
-			}		
+			}
 		}
-		$chprocess();
+		return $ret;
+	}
+
+	//提示文字的重载
+	//因为在bufficons_display_single()的时候已经把$bufficons_list[182]的值导入进$config了，在前面那个bufficons_check_buff_state()改动是没用的
+	function bufficons_display_single($token, $config, &$pa=NULL) {
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+		list($src, $config_ret) = $chprocess($token, $config, $pa);
+		if(182 == $token && \skillbase\skill_query(182,$pa)) {
+			eval(import_module('skill182'));
+			$config_ret['hint'] = $tmp_hint182;
+			$config_ret['activate_hint'] = '歌唱效果已经结束';
+		}
+		return Array($src, $config_ret);
 	}
 	
 	function show_songbuff_hint($lskey, $lsvalue)
@@ -108,7 +135,9 @@ namespace skill182
 			{
 				if ($lsvalue[$i] > 0) $o = '+';
 				else $o ='';
-				$s .= "{$ef_type[$key]}{$o}{$lsvalue[$i]} 剩<span class=\"yellow b\" id=\"songbuff{$i}\">{$uip['timing']['songbuff'.$i]['timing_r']}</span>秒<br>";
+				$timing_str = '';
+				if(!empty($uip['timing']['songbuff'.$i]['timing_r'])) $timing_str = $uip['timing']['songbuff'.$i]['timing_r'];
+				$s .= "{$ef_type[$key]}{$o}{$lsvalue[$i]} 剩<span class=\"yellow b\" id=\"songbuff{$i}\">$timing_str</span>秒<br>";
 			}
 		}
 		return $s;
