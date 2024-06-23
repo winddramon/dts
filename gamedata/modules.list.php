366153735de693593428aad692ca6b7264307f7a
satsuki8 <2893866331@qq.com>
2024-06-23 14:56:07 +0800

    add skill752

--- gamedata/modules.list.php
+++ gamedata/modules.list.php
@@ -309,6 +309,7 @@ skill744,extra/card/skills/skill744/,1
 skill_temp,extra/skill_temp/,1
 skill606,extra/club/skills/skill606/,1
 skill751,extra/card/skills/skill751/,1
+skill752,extra/card/skills/skill752/,1
 wep_g,base/weapon/wep_g/,1
 wep_j,base/weapon/wep_j/,1
 skill273,extra/club/skills/skill273/,1
