c56b1819ec39570f114d4fe06413251671202a6c
satsuki8 <2893866331@qq.com>
2024-06-29 00:01:15 +0800

    update

--- include/modules/extra/instance/instance12_dream/skill981/main.php
+++ include/modules/extra/instance/instance12_dream/skill981/main.php
@@ -39,7 +39,7 @@ namespace skill981
 			{
 				eval(import_module('logger'));
 				$stage = (int)\skillbase\skill_getvalue(981,'stage');
-				$theitem = array('itm'=>$stage.'级奖励盒','itmk'=>'Y','itme'=>1,'itms'=>3,'itmsk'=>$stage);
+				$theitem = array('itm'=>'梦境礼盒','itmk'=>'Y','itme'=>$stage,'itms'=>3,'itmsk'=>'');
 				\skill952\skill952_sendin_core($theitem, $pa);
 				$log .= "<span class=\"lime b\">奖励道具被送到了你的奖励箱中。</span><br>";
 			}
@@ -93,6 +93,125 @@ namespace skill981
 			return;
 		}
 		$chprocess();
+	}
+	
+	function itemuse(&$theitem)
+	{
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+		
+		eval(import_module('sys','player','logger'));
+		
+		$itm=&$theitem['itm']; $itmk=&$theitem['itmk'];
+		$itme=&$theitem['itme']; $itms=&$theitem['itms']; $itmsk=&$theitem['itmsk'];
+		
+		if (strpos($itmk, 'Y') === 0 && $itm == '梦境礼盒') 
+		{
+			$log .= "你使用了<span class=\"yellow b\">{$itm}</span>。<br>";
+			$pbx_choice = get_var_input('pbx_choice');
+			if (!empty($pbx_choice))
+			{
+				$pbx_choice = (int)$pbx_choice;
+				$pbx_itemlist = \skill981\get_prizebox_itemlist($itme, $itmsk);
+				if (!isset($pbx_itemlist[$pbx_choice-1]))
+				{
+					$log .= '参数不合法。<br>';
+					$mode = 'command';
+					return;
+				}
+				$prizeitem = $pbx_itemlist[$pbx_choice-1];
+				\skill952\skill952_sendin_core($prizeitem, $pa);
+				$log .= "<span class=\"yellow b\">{$prizeitem['itm']}</span>被送到了你的奖励箱中。<br>";
+				\itemmain\itms_reduce($theitem);
+				$itmsk = '';
+			}
+			if (empty($pbx_choice) || $itms > 0)
+			{
+				ob_start();
+				include template(MOD_SKILL981_USE_PRIZEBOX);
+				$cmd = ob_get_contents();
+				ob_end_clean();
+			}
+			return;
+		}
+		$chprocess($theitem);
+	}
+	
+	function get_prizebox_itemlist($itme, &$itmsk)
+	{
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+		if (!\itemmain\check_in_itmsk('^pbx', $itmsk)) 
+		{
+			$pbx_itemlist = add_prizebox_choices($itme, $itmsk);
+		}
+		$pbx_str = \itemmain\check_in_itmsk('^pbx', $itmsk);
+		$pbx_str = \attrbase\base64_decode_comp_itmsk($pbx_str);
+		$pbx_arr = explode(',',$pbx_str);
+		$pbx_itemlist = array();
+		eval(import_module('skill981'));
+		foreach($pbx_arr as $v)
+		{
+			if (is_numeric($v) && isset($skill981_prizeitems[$itme][(int)$v]))
+			{
+				$pi = $skill981_prizeitems[$itme][(int)$v];
+				$newitem = array('itm'=>$pi[0],'itmk'=>$pi[1],'itme'=>$pi[2],'itms'=>$pi[3],'itmsk'=>$pi[4]);
+			}
+			elseif (strpos($itmk, 's') === 0) $newitem = array('itm'=>'技能芯片','itmk'=>'VS','itme'=>1,'itms'=>1,'itmsk'=>substr($v,1));
+			elseif (strpos($itmk, 'c') === 0) $newitem = array('itm'=>'卡片福袋','itmk'=>'VO1','itme'=>1,'itms'=>1,'itmsk'=>substr($v,1));
+			$pbx_itemlist[] = $newitem;
+		}
+		return $pbx_itemlist;
+	}
+	
+	function add_prizebox_choices($itme, &$itmsk)
+	{
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+		eval(import_module('skill981'));
+		$pbx_pool = $skill981_prizeitems[$itme];
+		$pbx_arr = [];
+		for ($i = 0; $i < min(count($pbx_pool), 4); $i++)
+		{
+			if (empty($pbx_pool)) break;
+			$pbx_arr[] = get_prizeitem($pbx_pool);
+		}
+		$pbx_str = implode(',',$pbx_arr);
+		$itmsk .= '^pbx_'.\attrbase\base64_encode_comp_itmsk($pbx_str).'1';
+	}
+	
+	function get_prizeitem(&$arr)
+	{
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+		$wtotal = array_sum(array_column($arr, 5));
+		$wr = rand(1, $wtotal);
+		$w = 0;
+		foreach ($arr as $k => $v) {
+			$w += $v[5];
+			if ($wr <= $w) {
+				unset($arr[$k]);
+				if (strpos($v[4], 'rdsk_') === 0)
+				{
+					eval(import_module('item_randskills'));
+					$rdsklvl = $v[4][5];
+					return 's'.array_randompick($rs_cardskills[$rdsklvl]);
+				}
+				if (strpos($v[4], 'card_') === 0)
+				{
+					eval(import_module('cardbase'));
+					$rare = $v[4][5];
+					return 'c'.array_randompick($cardindex[$rare]);
+				}
+				return $k;
+			}
+		}
+	}
+	
+	//判定复合属性是否显示
+	function check_comp_itmsk_visible($cinfo){
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+		$ret = $chprocess($cinfo);
+		if ($ret) {
+			if (strpos($cinfo[0], '^pbx') === 0) return false;
+		}
+		return $ret;
 	}
 	
 	function parse_news($nid, $news, $hour, $min, $sec, $a, $b, $c, $d, $e, $exarr = array())
--- include/modules/extra/instance/instance12_dream/skill981/module.inc.php
+++ include/modules/extra/instance/instance12_dream/skill981/module.inc.php
@@ -3,11 +3,11 @@
 namespace skill981
 {
 	////////// MODULE HEADER START ///////////////
-	$___MODULE_dependency = 'skillbase clubbase sys player logger itemmain randnpc attack skill952';
-	$___MODULE_dependency_optional = '';
+	$___MODULE_dependency = 'skillbase clubbase sys player cardbase logger itemmain attrbase randnpc attack skill952';
+	$___MODULE_dependency_optional = 'item_recipe';
 	$___MODULE_conflict = '';
 	$___MODULE_codelist = 'main.php config/skill981.config.php';
-	$___MODULE_templatelist = 'desc showtrainstage';
+	$___MODULE_templatelist = 'desc showtrainstage use_prizebox';
 	////////// MODULE HEADER END /////////////////
 	require __INIT_MODULE__(__NAMESPACE__,__DIR__);
 }
