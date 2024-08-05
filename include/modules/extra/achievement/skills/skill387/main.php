3626d61c813908106dc4273e25c286e92f399c66
satsuki8 <2893866331@qq.com>
2024-08-05 19:35:23 +0800

    inst12 upd

---
+++ include/modules/extra/achievement/skills/skill387/main.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace skill387
+{
+	//各级要完成的成就名，如果不存在则取低的
+	$ach387_name = array(
+		1=>'10:00 P.M.',
+		2=>'0:30 A.M.',
+		3=>'3:59 A.M.',
+	);
+	
+	//各级显示的要求，如果不存在则取低的
+	$ach387_desc= array(
+		1=>'在<span class="lime b">梦境演练</span>中获胜时结算分数达到<:threshold:>分',
+	);
+	
+	$ach387_proc_words = '最高纪录';
+	
+	$ach387_unit = '分';
+	
+	//各级阈值，注意是达到这个阈值则升到下一级
+	$ach387_threshold = array(
+		1 => 11,
+		2 => 22,
+		3 => 40,
+		999 => NULL
+	);
+	
+	//各级给的切糕奖励
+	$ach387_qiegao_prize = array(
+		1 => 1337,
+		2 => 2333,
+		3 => 5120
+	);
+	
+	function init() 
+	{
+		define('MOD_SKILL387_INFO','achievement;');
+		define('MOD_SKILL387_ACHIEVEMENT_ID','87');
+	}
+	
+	function acquire387(&$pa)
+	{
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+	}
+	
+	function lost387(&$pa)
+	{
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+	}
+	
+	function ach_finalize_process(&$pa, $data, $achid)
+	{
+		if (eval(__MAGIC__)) return $___RET_VALUE;
+		$ret = $chprocess($pa, $data, $achid);
+		if($achid == 387){
+			eval(import_module('sys'));
+			if(\sys\is_winner($pa['name'],$winner))
+			{
+				$inst12_score = \instance12\inst12_get_score($pa);
+				if ($inst12_score > $ret)
+				{
+					$ret = $alvl;
+				}
+			}
+		}
+		return $ret;
+	}
+}
+
+?>
