fdf40ae90dcb65e6a301895e61a7f925708aba36
winddramon <ranshiin@qq.com>
2024-08-10 09:44:36 +0800

    v2.4.1

--- include/modules/core/sys/config/version.config.php
+++ include/modules/core/sys/config/version.config.php
@@ -1,6 +1,6 @@
 <?php
 //游戏版本
-$gameversion = 'N.E.W. v2.3.11';
+$gameversion = 'N.E.W. v2.4.1';
 //版权信息
 $gamecredits = <<<GAMECREDITS
 	<a href="https://github.com/winddramon/dts/">DTS $gameversion developed by sillycross, Lein, Yoshiko_G, NazoBlaster, </a><br>
