3a96e3bcf5b00c97ca3a3dfd1971f18c6b32f62f
satsuki8 <2893866331@qq.com>
2024-08-27 20:07:50 +0800

    upd

--- include/modules/extra/instance/instance12_dream/module.inc.php
+++ include/modules/extra/instance/instance12_dream/module.inc.php
@@ -3,7 +3,7 @@
 namespace instance12
 {
 	////////// MODULE HEADER START ///////////////
-	$___MODULE_dependency = 'sys player itemmain itemshop trap gameflow_combo npc skillbase cardbase map map_display metman event';
+	$___MODULE_dependency = 'sys player itemmain itemshop trap gameflow_combo npc skillbase cardbase map map_display metman event item_uee';
 	$___MODULE_dependency_optional = '';
 	$___MODULE_conflict = '';
 	$___MODULE_codelist = 'main.php config/plsinfo.config.php config/npc.data.config.php';
