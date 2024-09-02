3ab151afaf58b9014744dfd868cb532926240d64
satsuki8 <2893866331@qq.com>
2024-09-02 20:57:48 +0800

    fix

--- include/modules/extra/instance/instance12_dream/skill981/main.php
+++ include/modules/extra/instance/instance12_dream/skill981/main.php
@@ -206,6 +206,7 @@ namespace skill981
 					{
 						$money += $prizeitem['itme'];
 						$log .= "你获得了<span class=\"yellow b\">{$prizeitem['itme']}</span>元金钱。<br>";
+						\itemmain\itms_reduce($theitem);
 					}
 					else
 					{
